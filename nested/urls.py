22177bd46379bec411cc6030056b52b3b38910ad
kamol0007 <kamolbek00070007@gmail.com>
2024-11-20 00:41:18 +0500

    updated

--- nested/urls.py
+++ nested/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/generate-nested/', views.generate_nested, name='generate_nested'),
+    path('admin/generate-nested2/', views.generate_nested2, name='generate_nested2'),
 ]
--- nested/views.py
+++ nested/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import admin
 
-# Create your views here.
+
+def generate_nested(request):
+    messages.success(request, "Nested categories generated successfully.")
+    referer_url = request.META.get('HTTP_REFERER')
+
+    if referer_url:
+        return redirect(referer_url)
+    else:
+        return redirect('/admin/')
+
+
+@staff_member_required
+def generate_nested2(request):
+    messages.success(request, "Nested categories generated successfully.")
+
+    # Admin interfeysi uchun zarur kontekstni olish
+    admin_context = admin.site.each_context(request)
+
+    # Maxsus ma'lumotlarni qo'shish
+    admin_context.update({
+        'message': "Nested categories have been generated!",
+        'additional_data': "Here you can add extra details about the process.",
+    })
+
+    return render(request, 'admin/nested/generate_nested.html', admin_context)
